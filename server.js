80e828add4b34071012a10182454a7e9efc2aa3d
Joseph Choi <chousemath@gmail.com>
2016-04-30 15:17:00 +0900

    started backoffice in react

--- server.js
+++ server.js
@@ -8,24 +8,66 @@ var path = require("path");
 var db = require('./db.js');
 var port = process.env.PORT || 5000;
 
+
+// code to connect React
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
+app.use(express.static('./views'));
+
 app.use(bodyParser.json());
 
 app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/board.html'));
+  res.sendFile(path.join(__dirname, 'board.html'));
   //__dirname : It will resolve to your project folder.
 });
 
 app.get('/creative',function(req,res){
-  res.sendFile(path.join(__dirname+'/creative.html'));
+  res.sendFile(path.join(__dirname, 'creative.html'));
   //__dirname : It will resolve to your project folder.
 });
 
 app.get('/culinary',function(req,res){
-  res.sendFile(path.join(__dirname+'/culinary.html'));
+  res.sendFile(path.join(__dirname, 'culinary.html'));
   //__dirname : It will resolve to your project folder.
 });
 
-app.post("/subscription/new", function(req, res) {
+app.get('/backoffice', function(req, res) {
+  db.subscription.findAll().then(function(subscriptions) {
+    var subscriptionArray = [];
+    console.log(Array.isArray(subscriptionArray));
+    for (var subscription in subscriptions) {
+      if (subscriptions.hasOwnProperty(subscription)) {
+        
+        subscriptionArray.push(subscriptions[subscription].dataValues);
+
+
+        // console.log('\t' + subscriptions[subscription].dataValues.id);
+        // console.log('\t\t' + typeof(subscriptions[subscription].dataValues.id));
+        // console.log('\t' + subscriptions[subscription].dataValues.email);
+        // console.log('\t\t' + typeof(subscriptions[subscription].dataValues.email));
+        // console.log('\t' + subscriptions[subscription].dataValues.subscribed);
+        // console.log('\t\t' + typeof(subscriptions[subscription].dataValues.subscribed));
+        // console.log('\t' + JSON.stringify(subscriptions[subscription].dataValues.createdAt));
+        // console.log('\t\t' + typeof(JSON.stringify(subscriptions[subscription].dataValues.createdAt)));
+        // console.log('\t' + JSON.stringify(subscriptions[subscription].dataValues.updatedAt));
+        // console.log('\t\t' + typeof(JSON.stringify(subscriptions[subscription].dataValues.updatedAt)));
+      }
+    }
+    console.log(subscriptionArray);
+    console.log(Array.isArray(subscriptionArray));
+    res.render('backoffice.jsx', {subscriptions: subscriptionArray});
+  });
+});
+
+app.get('/subscriptions', function(req, res) {
+  db.subscription.findAll().then(function(subscriptions) {
+    res.send(subscriptions);
+  });
+});
+
+app.post("/subscriptions/new", function(req, res) {
   var body = _.pick(req.body, 'email');
   if (!_.isString(body.email) || body.email.trim().length === 0 || !_.contains(body.email, '@')) {
         res.status(400).json({"error": "The request body was not formatted correctly."});
@@ -64,7 +106,7 @@ app.get(/^(.+)$/, function(req, res){
 });
 
 
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({ force: false }).then(function() {
   app.listen(port, function() {
     console.log('server listening on port: ' + port);
   });
